6d7744a894b8a627844ea5f07db9355793cafc8f
AvivLevinson2001 <62341707+AvivLevinson2001@users.noreply.github.com>
2020-03-20 22:59:15 +0200

    commitmentmentment

--- app/src/main/java/com/example/sharonsimon/Fragments/ViewKenFragment.java
+++ app/src/main/java/com/example/sharonsimon/Fragments/ViewKenFragment.java
@@ -1,6 +1,9 @@
 package com.example.sharonsimon.Fragments;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.sharonsimon.Adapters.TaskAdapter;
 import com.example.sharonsimon.Classes.Ken;
 import com.example.sharonsimon.Classes.Task;
+import com.example.sharonsimon.Interfaces.FirebaseChangesListener;
 import com.example.sharonsimon.R;
 
 import java.util.ArrayList;
@@ -29,6 +33,19 @@ public class ViewKenFragment extends Fragment {
     TextView myKenNameTv;
     RecyclerView recycler;
     TaskAdapter adapter;
+
+    FirebaseChangesListener listener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Activity activity = (Activity)context;
+        try{
+            listener = (FirebaseChangesListener) activity;
+        }catch (ClassCastException e){
+            throw new ClassCastException("Activity: " + activity.toString() + " must implement KensRecyclerViewFragmentListener");
+        }
+    }
 
     public static ViewKenFragment newInstance(Ken ken, boolean isAdmin){
         ViewKenFragment fragment = new ViewKenFragment();
@@ -52,8 +69,35 @@ public class ViewKenFragment extends Fragment {
         myKenNameTv = viewGroup.findViewById(R.id.my_ken_name_tv);
         recycler = viewGroup.findViewById(R.id.tasks_rv);
 
-        ArrayList<Task> tasks = ken.getTasks();
+        final ArrayList<Task> tasks = ken.getTasks();
         adapter = new TaskAdapter(tasks);
+
+        adapter.setListener(new TaskAdapter.myTaskAdapterListener() {
+            @Override
+            public void onTaskClick(int position, View v) {
+
+            }
+
+            @Override
+            public void onTaskLongClick(int position, View v) {
+
+            }
+
+            @Override
+            public void onCheckBoxClick(int position, View v) {
+                tasks.get(position).setCompleted(!tasks.get(position).isCompleted());
+                adapter.notifyDataSetChanged();
+                ken.setTasks(tasks);
+                if(tasks.get(position).isCompleted())
+                    ken.setPoints(ken.getPoints() + tasks.get(position).getPoints());
+                else
+                    ken.setPoints(ken.getPoints() - tasks.get(position).getPoints());
+                listener.saveKenToFirebase(ken);
+                myKenPointsTV.setText(ken.getPoints() + "");
+            }
+        });
+
+        adapter.setCheckBoxIsClickable(isAdmin);
 
         recycler.setAdapter(adapter);
         recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
--- app/src/main/java/com/example/sharonsimon/Interfaces/FirebaseChangesListener.java
+++ app/src/main/java/com/example/sharonsimon/Interfaces/FirebaseChangesListener.java
@@ -1,10 +1,11 @@
 package com.example.sharonsimon.Interfaces;
 
 import com.example.sharonsimon.Classes.Ken;
+import com.example.sharonsimon.Classes.Task;
 
 import java.util.ArrayList;
 
 public interface FirebaseChangesListener {
-    void reloadInfoFromFirebase();
-    void saveKensToFirebase(ArrayList<Ken> kens);
+    void saveKenToFirebase(Ken kenToSave);
+    void addTasksToFirebase(ArrayList<Task> tasks);
 }
