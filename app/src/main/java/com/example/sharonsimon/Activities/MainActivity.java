967beae82645c8388b0b2a38e3796c64d7698e23
AvivLevinson2001 <62341707+AvivLevinson2001@users.noreply.github.com>
2020-03-30 16:31:20 +0300

    commitmentmentmentmentmentmentmentment

--- app/src/main/java/com/example/sharonsimon/Activities/MainActivity.java
+++ app/src/main/java/com/example/sharonsimon/Activities/MainActivity.java
@@ -176,13 +176,6 @@ public class  MainActivity extends AppCompatActivity implements KensRecyclerView
         });
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }*/
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == android.R.id.home){
